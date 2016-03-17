d94dc70372a1c02fc02931ab91505cb05f9fb9dc
AnNavara <Navarachkou@gmail.com>
2016-03-17 19:21:46 +0500

    Script and overall changes
    Added rebaseScript to slider
    Added ie9 matchMedia polyfill
    Added lazyload for second slider
    Fixed paddings mid resolution
    Redone sandwich
    Fixed global width to /4 without %
    Changed image size
    Mentions? @lumpur

--- source/js/script.js
+++ source/js/script.js
@@ -127,3 +127,21 @@
     return Math.floor(Math.random() * (max - min)) + min;
   }
 })();
+
+(function() {
+  function rebaseToSecond(el) {
+    if (el.parentNode.firstChild.nextSibling !== el) {
+      el.parentNode.insertBefore(el, el.parentNode.firstChild.nextSibling);
+    }
+  }
+
+  if (window.matchMedia('(max-width: 930px)').matches) {
+    var promo = document.querySelector('.promo');
+    var row = promo.querySelectorAll('.promo__row');
+
+    for (var i = 0; i < row.length; i++) {
+      var text = row[i].querySelector('.promo__text');
+      rebaseToSecond(text);
+    }
+  }
+})();
